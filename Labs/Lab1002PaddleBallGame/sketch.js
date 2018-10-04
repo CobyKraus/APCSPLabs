3bce5df2d8e8cb2a310673e42300259e3eecb444
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-10-04 08:49:48 -0700

    End of Day 1

--- Labs/Lab1002PaddleBallGame/sketch.js
+++ Labs/Lab1002PaddleBallGame/sketch.js
@@ -1,4 +1,5 @@
 //  Global variables
+
 var balls = [];
 
 // put setup code here
@@ -6,7 +7,7 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);
-  loadBalls(200);  //  !!!!!!!!!!  this is a function call
+  loadBalls(1);  //  !!!!!!!!!!  this is a function call
 }
 
 function draw() {

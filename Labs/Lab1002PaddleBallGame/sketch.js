3649efd78df031ab466fcd2e50cda1f39c0adda2
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-10-04 08:35:38 -0700

    New Changes

--- Labs/Lab1002PaddleBallGame/sketch.js
+++ Labs/Lab1002PaddleBallGame/sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //  Global variables
 var balls = [];
 
@@ -30,36 +29,3 @@ function loadBalls(numBalls){
     }
 
 }
-=======
-//  Global variables
-var balls = [];
-
-// put setup code here
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(20,20,20);
-  loadBalls(200);  //  !!!!!!!!!!  this is a function call
-}
-
-function draw() {
-  background(20,20,20);
-  for(var i = 0; i < balls.length; i = i + 1){
-    balls[i].run();
-  }
-}
-
-function loadBalls(numBalls){
-
-    for(var i = 0; i < numBalls; i++){
-        var loc = createVector(random(width), random(height));
-        var vel = createVector(random(-3,3), random(-3,3));
-        var radius = random(20,40);
-        var col = color(random(255), random(255), random(255));
-
-        balls.push(new Ball(loc, vel, radius, col));
-
-    }
-
-}
->>>>>>> e7cf9c56d6fbfc05183b66657b40d946e2cf36a5
--- Labs/Lab1002PaddleBallGame/square.js
+++ Labs/Lab1002PaddleBallGame/square.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 **  Ball Constructor Function
 **  Coby Kraus
@@ -31,37 +30,3 @@ function Ball(loc, rad, col){
    }
 
 }
-=======
-/*
-**  Ball Constructor Function
-**  Coby Kraus
-**  Aug 20, 2018
-*/
-
-function Ball(loc, rad, col){
-  // Instance variables
-   this.loc = loc;
-   this.rad = rad;
-   this.col = col;
-  // This function calls other functions
-  this.run = function(){
-    this.update();
-    this.render();
-  }
-  // This function changes the location of the ball
-  // by adding speed to x and y
-   this.update = function(){
-     var mouseLoc = createVector(mouseX, mouseY);
-     this.loc = p5.Vector.lerp(this.loc, mouseLoc, .09)
-
-   }
-
-
-    // render() draws the square at the new location
-   this.render = function(){
-      fill(0,255,0);
-      rect(this.loc.x,500 , 150, 20);
-   }
-
-}
->>>>>>> e7cf9c56d6fbfc05183b66657b40d946e2cf36a5

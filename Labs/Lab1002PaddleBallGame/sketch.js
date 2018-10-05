7af896ac887e41f60ebe1a0c5fcbb947ef151deb
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-10-05 08:50:33 -0700

    Paddle moves and beginning of splice

--- Labs/Lab1002PaddleBallGame/sketch.js
+++ Labs/Lab1002PaddleBallGame/sketch.js
@@ -1,19 +1,34 @@
 //  Global variables
 
 var balls = [];
-
+var square
 // put setup code here
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);
-  loadBalls(1);  //  !!!!!!!!!!  this is a function call
+  loadBalls(20);  //  !!!!!!!!!!  this is a function call
+  square = new Square(createVector(0, 0),
+                      33,
+                      color(random(255),random(255),random(255)))
+
 }
 
 function draw() {
   background(20,20,20);
+  square.run();
   for(var i = 0; i < balls.length; i = i + 1){
     balls[i].run();
+  }
+  for (var i = 0; i < balls.length; i++){
+    var x = balls[i];
+    var y = square.loc;
+    var z = x.loc;
+    //returns the distance between two objects
+    var dist = y.dist(z);
+    if (dist <= 50){
+    balls.splice(i, 1)// remove one element at index i
+    }
   }
 }
 

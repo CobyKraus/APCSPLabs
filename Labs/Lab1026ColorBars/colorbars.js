98cf0686e0fb4da4d6e078b9bd3e7c5297143f4b
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-10-30 08:49:30 -0700

    Continued

--- Labs/Lab1026ColorBars/colorbars.js
+++ Labs/Lab1026ColorBars/colorbars.js
@@ -3,7 +3,16 @@
 **  Coby Kraus
 **  Aug 20, 2018
 */
-function bar(length,width){
-  this.length=100;
-  this.width=Canvas.length/100;
+function Bar(loc,wid,hei,clr){
+  this.loc=location;
+  this.width=wid;
+  this.height=hei;
+  this.clr=color;
+
+
+
+  this.render = function(){
+    fill(this.clr);
+    rect(this.loc.x,this.loc.y,this.wid,this.hei)
+  }
 }
--- Labs/Lab1026ColorBars/sketch.js
+++ Labs/Lab1026ColorBars/sketch.js
@@ -5,11 +5,24 @@ var bars = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(20,20,20);
+  background(20,20,20,100);
+  numBars = 40;
+  LoadBars(numBars);
 }
-function ColorBars(){
-  for (var i=0;i<=Canvas.length/100;i++){
-    fill(random(0,255),random(0,255),random(0,255));
-    bars.push(new Bar(bar.length, bar.width));
+function LoadBars(numBars){
+  for (var i=0;i<numBars;i++){
+    var loc = createVector(100,100);
+    var wid = 800/numBars
+    var hei = 100
+    var blue = random(0,255);
+    var clr = (random(0,255),random(0,255),blue);
+    var bar = new Bar(loc,wid,hei,clr);
+    bars.push(bar);
+  }
+}
+function draw(){
+  noStroke();
+  for (var i=0;i<numBars;i++){
+    bars[i].render();
   }
 }

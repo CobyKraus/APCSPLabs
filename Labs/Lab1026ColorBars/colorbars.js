1a24ceba80fa9c36f53fb60acb1eaa622db240c5
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-10-31 08:50:45 -0700

    finished!

--- Labs/Lab1026ColorBars/colorbars.js
+++ Labs/Lab1026ColorBars/colorbars.js
@@ -4,15 +4,15 @@
 **  Aug 20, 2018
 */
 function Bar(loc,wid,hei,clr){
-  this.loc=location;
-  this.width=wid;
-  this.height=hei;
-  this.clr=color;
+  this.loc=loc;
+  this.wdth=wid;
+  this.hght=hei;
+  this.clr=clr;
 
 
 
   this.render = function(){
     fill(this.clr);
-    rect(this.loc.x,this.loc.y,this.wid,this.hei)
+    rect(this.loc.x,this.loc.y,this.wdth,this.hght)
   }
 }
--- Labs/Lab1026ColorBars/sketch.js
+++ Labs/Lab1026ColorBars/sketch.js
@@ -8,14 +8,15 @@ function setup() {
   background(20,20,20,100);
   numBars = 40;
   LoadBars(numBars);
+  bubbleSort(bars);
 }
 function LoadBars(numBars){
   for (var i=0;i<numBars;i++){
-    var loc = createVector(100,100);
+    var loc = createVector((wid*i),100);
     var wid = 800/numBars
     var hei = 100
-    var blue = random(0,255);
-    var clr = (random(0,255),random(0,255),blue);
+    var blue = random(255);
+    var clr = color(random(255),random(255),blue);
     var bar = new Bar(loc,wid,hei,clr);
     bars.push(bar);
   }
@@ -24,5 +25,18 @@ function draw(){
   noStroke();
   for (var i=0;i<numBars;i++){
     bars[i].render();
+  }
+}
+function bubbleSort(bars){
+
+  for(var i = bars.length - 1; i >= 0; i-- ){
+    //second for loop that goes from 0 to i
+    for(var j = 0; j < i; j++ ){
+      if (bars[i].blue<bars[j].blue){
+        var temp = bars[i];
+        bars[i] = bars[j];
+        bars[j] = temp
+      }
+    }
   }
 }

f3760d027dc782d6bbdc2ddf86e4d844f4a09b0e
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-11-02 08:48:53 -0700

    Getting closer

--- Labs/Lab1101APIS/sketch.js
+++ Labs/Lab1101APIS/sketch.js
@@ -1,9 +1,24 @@
 var data;
-var barHeight;
+var myArray;
 //preload a large file
 function preload(){
-  data = loadJSON("weather.json");
+  data = loadJSON("senators.json");
 }
 function setup(){
-  console.log(data.countries[0]);
+  myArray=data.objects
+  console.log(myArray);
+}
+
+function BubbleSort(){
+  var length = txt.length
+  for(var i = length - 1; i >= 0; i-- ){
+  //second for loop that goes from 0 to i
+  for(var j = 0; j < i; j++ ){
+    if (txt[i].person.lastname<txt[j].person.lastname){
+      var temp = txt[i].person.lastname;
+      txt[i].person.lastname = txt[j].person.lastname;
+      txt[j].person.lastname = temp
+    }
+  }
+}
 }

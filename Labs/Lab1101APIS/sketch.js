d61354af4a8f4c8094e78137e7159250ca553fb1
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-11-05 08:50:40 -0800

    confused

--- Labs/Lab1101APIS/sketch.js
+++ Labs/Lab1101APIS/sketch.js
@@ -1,24 +1,24 @@
 var data;
 var myArray;
-//preload a large file
-function preload(){
-  data = loadJSON("senators.json");
-}
+
+
 function setup(){
-  myArray=data.objects
-  console.log(myArray);
+  loadJSON("file://///10.7.1.51/E17%20share%20folder/Principles/populationData/population.json", gotData, 'jsonp');
 }
 
+function gotData(dataNew){
+  data = dataNew;
+}
 function BubbleSort(){
   var length = txt.length
   for(var i = length - 1; i >= 0; i-- ){
-  //second for loop that goes from 0 to i
-  for(var j = 0; j < i; j++ ){
-    if (txt[i].person.lastname<txt[j].person.lastname){
-      var temp = txt[i].person.lastname;
-      txt[i].person.lastname = txt[j].person.lastname;
-      txt[j].person.lastname = temp
+    //second for loop that goes from 0 to i
+    for(var j = 0; j < i; j++ ){
+      if (txt[i].population.males<txt[j].population.males){
+        var temp = txt[i].person.lastname;
+        txt[i].population.males = txt[j].population.males;
+        txt[j].population.males = temp
+      }
     }
   }
-}
 }

e98aea95abadca57bb8225249d5fab8b83d81ffc
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-11-06 08:49:37 -0800

    better

--- Labs/Lab1101APIS/sketch.js
+++ Labs/Lab1101APIS/sketch.js
@@ -1,23 +1,38 @@
 var data;
-var myArray;
-
-
+var barHeight;
 function setup(){
-  loadJSON("file://///10.7.1.51/E17%20share%20folder/Principles/populationData/population.json", gotData, "jsonp");
+  var cnv = createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
+  background(20);
+  barHeight=20;
+  loadJSON("population.json", gotData);
 }
-
+//function draw(){
+  //useData();
+//}
 function gotData(dataNew){
   data = dataNew;
 }
+/*
+function useData(){
+  if(data){
+    BubbleSort();
+    for(var i =0; i<;i++){
+
+
+    }
+  }
+}
+*/
 function BubbleSort(){
-  var length = txt.length
+  var length = data.countrydata.length;
   for(var i = length - 1; i >= 0; i-- ){
     //second for loop that goes from 0 to i
     for(var j = 0; j < i; j++ ){
-      if (txt[i].population.males<txt[j].population.males){
-        var temp = txt[i].person.lastname;
-        txt[i].population.males = txt[j].population.males;
-        txt[j].population.males = temp
+      if (data.countrydata.males[i] < data.countrydata.males[j]){
+        var temp = data.countrydata.males[i];
+        data.countrydata.males[i] = data.countrydata.males[j];
+        data.countrydata.males[j] = temp
       }
     }
   }

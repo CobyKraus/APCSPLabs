fa40dc8499c37667f2ce8056315bffe71eec6a07
CobyKraus <42615091+CobyKraus@users.noreply.github.com>
2018-11-15 08:51:05 -0800

    got the shape to turnnnnnn

--- Labs/Lab1114SnakeGame/food.js
+++ Labs/Lab1114SnakeGame/food.js
@@ -3,3 +3,7 @@
 **  Coby Kraus
 **  Aug 20, 2018
 */
+function DrawRectangle(){
+
+  rect(random(0,800),random(0,800),20,20);
+}
--- Labs/Lab1114SnakeGame/sketch.js
+++ Labs/Lab1114SnakeGame/sketch.js
@@ -17,4 +17,13 @@ function keyPressed(){
   if(keyCode===UP_ARROW){
     snake.vel = createVector(0,-1);
   }
+  if(keyCode===DOWN_ARROW){
+    snake.vel = createVector(0,1);
+  }
+  if(keyCode===RIGHT_ARROW){
+    snake.vel = createVector(1,0);
+  }
+  if(keyCode===LEFT_ARROW){
+    snake.vel = createVector(-1,0);
+  }
 }
